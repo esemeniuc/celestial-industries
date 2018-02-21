08573fde7a9a561a72669845ac09f847dd0e82fb
Ahmed Khalifa <ahmdk@users.noreply.github.com>
2018-02-21 01:39:33 -0800

    Data structure to represent a level map as a set of tile costs   * this will be fed to the A* search algorithm

--- src/level.cpp
+++ src/level.cpp
@@ -35,13 +35,14 @@ std::vector<std::vector<int>> Level::levelLoader(std::string levelTextFile)
 {
 	std::ifstream level (levelTextFile);
 	std::string line;
-	std::vector<std::vector<int>> levelData;
+	std::vector<std::vector<int>> levelData;	
 
 	if (level.is_open())
 	{
 		while (getline(level, line))
 		{
 			std::vector <int> row;
+			std::vector<float> costs;
 			for (char& tile : line) 
 			{
 				switch (tile)
@@ -49,27 +50,36 @@ std::vector<std::vector<int>> Level::levelLoader(std::string levelTextFile)
 					case '#':
 					{
 						row.push_back(BRICK_CUBE);
+						costs.push_back(1000.0);
 						break;
 					}
 
 					case ' ':
 					{
 						row.push_back(SAND_1);
+						costs.push_back(1.0);
 						break;
 					}
 					default:
 					{
 						row.push_back(SAND_2);
+						costs.push_back(1.0);
 						break;
 					}
 				}
 			}
 			levelData.push_back(row);
+			levelTraversalCostMap.push_back(costs);
 		}
 		level.close();
 	}
 	else fprintf(stderr, "Failed to open level data file");
 	return levelData;
+}
+
+std::vector<std::vector<float>> Level::getLevelTraversalCostMap()
+{
+	return this->levelTraversalCostMap;
 }
 
 bool Level::initTileTypes(std::vector<std::tuple<TileType, std::string>> sources)
--- src/level.hpp
+++ src/level.hpp
@@ -26,9 +26,11 @@ class Level
 public:
 	bool init(std::vector<std::vector<int>> intArray, std::vector<std::tuple<TileType, std::string>> sources);
 	std::vector<std::vector<int>> levelLoader(std::string levelTextFile);
-
 	std::vector<std::vector<Tile>> tiles; // we can add the time dimension when we get there
 	std::map<TileType, OBJ::Data> tileTypes;
+	std::vector < std::vector<float>> getLevelTraversalCostMap();
+
 private:
 	bool initTileTypes(std::vector<std::tuple<TileType, std::string>> sources);
+	std::vector<std::vector<float>> levelTraversalCostMap;
 };
