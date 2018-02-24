71df17c996c58946f6cbd630fa9f11fc9dc07fd3
Andres Rama <andresramacastel@gmail.com>
2018-02-24 14:29:36 -0500

    Fix for build 1

--- src/tile.cpp
+++ src/tile.cpp
@@ -23,13 +23,14 @@ void GunTowerTile::update(float ms)
     timeCounter += ms;
     float period = 50;
     glm::vec3 gunDisplacement = { 0, 0, 0.1 };
+    float amountToMove = sin(timeCounter / period);
     setModelMatrix(
         2,
-        gunDisplacement*sin(timeCounter / period) + glm::vec3({0,0,0.1})
+        gunDisplacement*amountToMove + glm::vec3({0,0,0.1})
     );
     setModelMatrix(
         3,
-        -gunDisplacement*sin(timeCounter / period) + glm::vec3({ 0,0,0.1 })
+        -gunDisplacement*amountToMove + glm::vec3({ 0,0,0.1 })
     );
 
     rotate(1, ms/1000, { 0,1,0 });
