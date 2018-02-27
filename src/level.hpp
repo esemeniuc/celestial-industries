61051a02a88112776c7469cadf6fbf180edc103a
esemeniuc <3838856+esemeniuc@users.noreply.github.com>
2018-02-26 19:06:19 -0800

    use unordered map and some reformat

--- src/level.hpp
+++ src/level.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <fstream>
-#include <map>
 #include <limits>
 #include <ostream>
+#include <unordered_map>
 #include "tile.hpp"
 
 #define INF std::numeric_limits<float>::infinity()
@@ -22,7 +21,7 @@ enum class TileType {
 	MINING_TOWER,
 	PHOTON_TOWER,
 	TREE,
-    GUN_TURRET,
+	GUN_TURRET,
 };
 
 // used to build a graph of nodes for the AI pathfinder to traverse each tile node.
@@ -33,9 +32,9 @@ struct AStarNode {
 	AStarNode() = default;
 
 	AStarNode(int _rowCoord, int _colCoord, int _movementCost, float _fScore) : rowCoord(_rowCoord),
-																						colCoord(_colCoord),
-																						movementCost(_movementCost),
-																						fScore(_fScore) {}
+																				colCoord(_colCoord),
+																				movementCost(_movementCost),
+																				fScore(_fScore) {}
 
 	bool operator==(const AStarNode& rhs) const {
 		return rowCoord == rhs.rowCoord &&
@@ -66,10 +65,10 @@ struct Coord {
 class Level {
 public:
 	//members
-    // Using a shared pointer to a tile allows us to actually have derived classes in there as well.
+	// Using a shared pointer to a tile allows us to actually have derived classes in there as well.
 	std::vector<std::vector<std::shared_ptr<Tile>>> tiles; // we can add the time dimension when we get there
-	std::map<TileType, std::vector<SubObject>> tileTypes;
-    std::map<TileType, std::shared_ptr<CompositeObjectBulkRenderer>> tileRenderers;
+	std::unordered_map<TileType, std::vector<SubObject>> tileTypes;
+	std::unordered_map<TileType, std::shared_ptr<CompositeObjectBulkRenderer>> tileRenderers;
 
 	//funcs
 	bool init(
@@ -80,9 +79,12 @@ public:
 
 	void update(float ms);
 
-    std::vector<std::vector<TileType>> levelLoader(const std::string& levelTextFile);
-    std::vector<std::vector<AStarNode>> getLevelTraversalCostMap();
 	bool displayPath(const std::vector<Coord>& levelArray);
+
+	std::vector<std::vector<TileType>> levelLoader(const std::string& levelTextFile);
+
+	std::vector<std::vector<AStarNode>> getLevelTraversalCostMap();
+
 private:
 	//members
 	std::vector<std::vector<AStarNode>> levelTraversalCostMap;
