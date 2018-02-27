0d659f118f6b9b75d29e98228ee4e082146acd49
esemeniuc <3838856+esemeniuc@users.noreply.github.com>
2018-02-26 18:39:53 -0800

    some states for buildings

--- src/building.h
+++ src/building.h
@@ -5,8 +5,19 @@
 
 #include <entity.hpp>
 
-class Building : Entity{
+class Building : Entity {
+protected:
 	int health;
+
+};
+
+
+enum class DefenceBuildingState {
+	IDLE, PIVOT, ATTACK
+};
+
+class DefenceBuilding : Building {
+protected:
 	int attackPower;
 	int attackRange;
 	int attackSpeed;
