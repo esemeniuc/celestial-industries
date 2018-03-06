8e365f2bc16befac1f64deb7bb94fb34eae1fb41
Ahmed Khalifa <ahmdk@users.noreply.github.com>
2018-03-06 14:50:30 -0800

    First pass at refactoring entity

--- src/entity.hpp
+++ src/entity.hpp
@@ -4,109 +4,26 @@
 #include <cmath>
 #include <sstream>
 #include <cstring>
+#include <map>
 
 // glm
-#define GLM_ENABLE_EXPERIMENTAL
-
 #include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtx/transform.hpp"
-#include "glm/gtx/quaternion.hpp"
 
 // custom headers
 #include "objloader.hpp"
 #include "renderer.hpp"
 
-// collision geometries
-struct bounding_box {
-
-//               	 @ + + + + + + + + @						|
-//               	 +\                +\						|
-//               	 + \               + \						|
-//               	 +  \              +  \         y			|
-//           width   +   \             +   \        |			|
-//               	 +    @ + + + + + +++ + @       |______ x	|
-//               	 +    +            +    +        \			|
-//               	 +    +            +    +         \			|
-//               	 +    +            +    +          z		|
-//  corner position  @ + +++ + + + + + @    +					|
-//               	  \   +             \   +					|
-//               	   \  +              \  +					|
-//               height \ +               \ +					|
-//               	     \+    length      \+					|
-//               	      @ + + + + + + + + @					|
-
-	double length, width, height;
-	glm::vec3 corner_position;
-};
-
-struct bounding_sphere {
-	double radius;
-	glm::vec3 center;
-};
-
-struct bounding_cylinder {
-	double radius, height;
-	glm::vec3 center;
-};
-
-enum collision_geometry_type {
-	cg_bounding_box,
-	cg_bounding_sphere,
-	cg_bounding_cylinder,
-};
 
 enum class EntityOwner {
 	NONE, PLAYER, AI
 };
 
-
 class Entity : public Renderable {
 public:
 	explicit Entity(const std::shared_ptr<Renderer> &initParent);
-
-	virtual void update(float ms) = 0;
-	void setVelocity (glm::vec3);
-	void setGravity (glm::vec3);
-	void setForce (glm::vec3);
-	void setGeometryId(long);
-	void setRotation (glm::vec3);
-	void setTranslation (glm::vec3);
-	void setScale(glm::vec3);
-	void setPosition(glm::vec3);
-	void getCameraPosition(glm::vec3);
-	glm::vec3 getPosition();
-	void applyTransformations();
-	void setCollisionGeometryType(collision_geometry_type);
-	glm::mat4 getModelMatrix();
-	collision_geometry_type getCollisionGeometryType();
-	glm::vec3 getVelocity();
-	long getGeometryId();
-	bool isTextured();
+	virtual void update(float ms);
 
 protected:
-	// physical properties
-	double density;
-	double volume;
-	glm::vec3 velocity;
-	glm::vec3 gravity;
-	glm::vec3 applied_force;
-	glm::vec3 translation = glm::vec3(0.0, 0.0, 0.0);
-	glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0);
-	glm::vec3 rotation = glm::vec3(0.0, 0.0,0.0);
-	glm::vec3 position;
-	glm::vec3 cameraPosition;
-	glm::mat4 model = glm::mat4(1.0);
-	
-	// collision geometry type
-	collision_geometry_type cg_type;
-
-	// graphics attributes
-	size_t indices;
-	bool texture_flag;
-
-	// id number of the geometry
-	long geometry_id;
-
+    std::map<Config::MeshType, std::shared_ptr<Renderer>> meshRenderers;
 	EntityOwner owner;
 };
