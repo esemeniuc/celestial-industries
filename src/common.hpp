5f0e34f0100127f42eb4559271b1dcb313b8c4ad
Ahmed Khalifa <ahmdk@users.noreply.github.com>
2018-03-07 00:57:24 -0800

    Move shader loading code to shader.cpp and hpp

--- src/common.hpp
+++ src/common.hpp
@@ -95,18 +95,6 @@ struct Mesh
 	OBJ::Material material;
 };
 
-// Container for Vertex and Fragment shader, which are then put(linked) together in a
-// single program that is then bound to the pipeline.
-struct Shader
-{
-	bool load_from_file(const char* vs_path, const char* fs_path);
-	void release();
-
-	GLuint vertex;
-	GLuint fragment;
-	GLuint program;
-};
-
 struct Coord {
 	int rowCoord, colCoord;
 
