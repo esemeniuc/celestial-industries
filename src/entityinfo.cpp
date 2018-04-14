8775b23c1eea11b485053cbda2f03e1f44365e39
Eric <cwaffles@putdispenserhere.com>
2018-04-13 23:28:48 -0700

    small cleanups

--- src/entityinfo.cpp
+++ src/entityinfo.cpp
@@ -5,13 +5,18 @@
 #include "entityinfo.hpp"
 std::array<const char*, Model::MeshType::MESHTYPES_COUNT> init() {
 	std::array<const char*, Model::MeshType::MESHTYPES_COUNT> tempNameLookupTable{};
+	//buildings
 	tempNameLookupTable[Model::MeshType::REFINERY] = "Refinery";
 	tempNameLookupTable[Model::MeshType::MINING_TOWER] = "Mining Tower";
 	tempNameLookupTable[Model::MeshType::PHOTON_TOWER] = "Photon Tower";
 	tempNameLookupTable[Model::MeshType::SUPPLY_DEPOT] = "Supply Depot";
-	tempNameLookupTable[Model::MeshType::BALL] = "Ball";
 	tempNameLookupTable[Model::MeshType::GUN_TURRET] = "Gun Turret";
+
+	//geyser
 	tempNameLookupTable[Model::MeshType::GEYSER] = "Resource Geyser";
+
+	//units
+	tempNameLookupTable[Model::MeshType::BALL] = "Ball";
 	tempNameLookupTable[Model::MeshType::ENEMY_SPIKE_UNIT] = "Spiked Melee";
 	tempNameLookupTable[Model::MeshType::ENEMY_RANGED_LINE_UNIT] = "Line Attack";
 	tempNameLookupTable[Model::MeshType::ENEMY_RANGED_RADIUS_UNIT] = "Splash Attack";
